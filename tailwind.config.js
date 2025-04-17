b3f20bf859c37a237ef18aeb3777316baab30bbc
sriraksha166 <srirakshakundar475@gmail.com>
2025-04-17 11:20:10 +0530

    news and latest post

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+
 module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
-        roc: ['"Roc Grotesk"', 'sans-serif'],
+        Mulish: ['Mulish', 'sans-serif'],
       },
     },
   },
-};
+  plugins: [],
+}
