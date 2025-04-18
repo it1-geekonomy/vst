e4fc7915e3610d7da7e5d9b6989f330a25265ffe
Thushara <thushara@geekonomy.in>
2025-04-18 13:50:50 +0530

    initiatives

--- src/components/CSR.tsx
+++ src/components/CSR.tsx
@@ -354,8 +354,11 @@ const CSR = () => {
                         </AnimatePresence>
                     </section>
                 </motion.div>
-            </main>
-            <Footer />
+                        </main>
+                        <div className='py-14 bg-black'>
+                            <Initiatives />
+            </div>
+            <Footer bgcolour="bg-black" />
         </div>
     );
 };
