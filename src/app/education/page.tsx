26a0fe6d9a0c32a670725d9e165db8b7c5fc8570
roshan <roshan@geekonomy.in>
2025-04-22 11:30:30 +0530

    change

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -106,7 +106,6 @@ export default function EducationPage() {
                   />
                 </div>
                 <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/70 to-transparent py-2">
-                  <p className="text-white text-xs sm:text-sm text-center">{image.alt}</p>
                 </div>
               </div>
             ))}
