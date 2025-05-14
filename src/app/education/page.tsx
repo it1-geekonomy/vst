c679115d1252b5a369065ea994e5867406a4ad0d
sachingeekonomy <sachin@geekonomy.in>
2025-05-14 13:05:25 +0530

    our location changed

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -231,7 +231,7 @@ export default function EducationPage() {
                 className={`relative overflow-hidden transition-all duration-1000 ease-in-out ${showAllImages ? 'flex-[3]' : index === activeDesktopImage ? 'flex-[35]' : 'flex-[3]'
                   }`}
                 style={{
-                  height: showAllImages ? image.height : index === activeDesktopImage ? '86%' : image.height,
+                  height: showAllImages ? image.height : index === activeDesktopImage ? '94%' : image.height,
                   transition: 'all 1s ease-in-out'
                 }}
               >
@@ -308,26 +308,25 @@ export default function EducationPage() {
         </div>
       </div>
 
-
       {/* Our Location section - using the new component */}
-      <div className="-ml-80 sm:-ml-[28rem] md:-ml-[36rem] lg:-ml-[44rem]">
-      <LocationSection
-        locationImage="education/educational location.png"
-        address={{
-          street: "Edward Road,",
-          street2: "Off Queens Road,",
-          city: "Bangalore-560 001",
-          state: "Karnataka.",
-          pincode: ""       
-        }}
-        phoneNumbers={[
-          '+91 80 2234 1011',
-          '+91 80 2226 3022',
-          '+91 99807 97527'
-        ]}
-        googleMapsUrl="https://www.google.com/maps/place/SKEI+-+Smt.+Kamalabai+Educational+Institution/@12.987965,77.5947328,17z/data=!3m1!4b1!4m6!3m5!1s0x3bae1667c5c960f1:0x4e3200223320b7c2!8m2!3d12.987965!4d77.5973077!16s%2Fg%2F1t_kdz9b?entry=ttu&g_ep=EgoyMDI1MDUwNy4wIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D"
-      />
-        </div>
+      <div className="w-full px-4 md:px-4 lg:px-4">
+        <LocationSection
+          locationImage="education/educational location.png"
+          address={{
+            street: "Edward Road,",
+            street2: "Off Queens Road,",
+            city: "Bangalore-560 001",
+            state: "Karnataka.",
+            pincode: ""       
+          }}
+          phoneNumbers={[
+            '+91 80 2234 1011',
+            '+91 80 2226 3022',
+            '+91 99807 97527'
+          ]}
+          googleMapsUrl="https://www.google.com/maps/place/SKEI+-+Smt.+Kamalabai+Educational+Institution/@12.987965,77.5947328,17z/data=!3m1!4b1!4m6!3m5!1s0x3bae1667c5c960f1:0x4e3200223320b7c2!8m2!3d12.987965!4d77.5973077!16s%2Fg%2F1t_kdz9b?entry=ttu&g_ep=EgoyMDI1MDUwNy4wIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D"
+        />
+      </div>
       <div className="-mt-20">
         <div className="transform scale-110">
           <BusinessSectorsUpdated />
