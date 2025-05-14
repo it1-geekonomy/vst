47233b389ee3799440cb78c8614f55cbcad2764b
Roshan <1js20is089@gmail.com>
2025-05-15 01:39:27 +0530

    car mt

--- src/components/automotiveFranchises/franchiesMain.tsx
+++ src/components/automotiveFranchises/franchiesMain.tsx
@@ -873,7 +873,7 @@ const FranchiseSlider = () => {
   const currentLocations = slides[currentSlide].locations;
 
   return (
-    <div className="relative w-full min-h-screen  lg:mt-[0rem] mt-[1rem]">
+    <div className="relative w-full min-h-screen  lg:mt-[-3rem] mt-[1rem]">
       {/* Fixed background gradient */}
       <div
         className="fixed inset-0 z-0 pointer-events-none"
@@ -899,7 +899,7 @@ const FranchiseSlider = () => {
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
                 transition={{ duration: 0.5 }}
-                className="absolute left-0 w-[50%] lg:w-[92%] h-[90%] rounded-br-[120px] overflow-hidden"
+                className="absolute left-0 w-[50%] lg:w-[92%] h-full rounded-br-[120px] overflow-hidden"
               >
                 <Image
                   src={slides[currentSlide].backgroundImage}
