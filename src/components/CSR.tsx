21104013fc3f781cd1c89049fde81e530d38330c
Roshan <1js20is089@gmail.com>
2025-05-14 16:32:25 +0530

    make change

--- src/components/CSR.tsx
+++ src/components/CSR.tsx
@@ -403,7 +403,7 @@ const CSR = () => {
             </div>
           </section>
           {/* Initiatives Section */}
-          <div className="w-full py-6 sm:py-4 md:py-6 lg:py-8 pl-0 sm:pl-0 md:pl-15 lg:pl-20 xl:pl-[10rem] h-[1080px]:pl-0">
+          <div className="w-full py-6 sm:py-4 md:py-6 lg:py-8 pl-0 sm:pl-0 ">
             <Initiatives />
           </div>
 
