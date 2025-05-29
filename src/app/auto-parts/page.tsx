d3a68e0ba4991b5762c08c3fd8f01a30800fdf34
sachingeekonomy <sachin@geekonomy.in>
2025-05-29 12:48:29 +0530

    our location changes in education and auot parts

--- src/app/auto-parts/page.tsx
+++ src/app/auto-parts/page.tsx
@@ -529,9 +529,9 @@ export default function Page() {
             locationImage="vst-auto-parts/location img.png"
             address={{
               street: "Old No.144, New No.199,",
-              street2: "1st Floor, Anna Salai, Chennai-",
-              state: "",
-              city: "600 002, Tamil Nadu.",
+              street2: "1st Floor, Anna Salai,",
+              state: "Chennai - 600 002, Tamil Nadu.",
+              city: "",
               pincode: "",
             }}
             phoneNumbers={[
