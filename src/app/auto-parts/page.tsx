26247141cdf711e5d19a653c49d176ff7a88464c
Roshan <1js20is089@gmail.com>
2025-06-02 19:35:12 +0530

    a

--- src/app/auto-parts/page.tsx
+++ src/app/auto-parts/page.tsx
@@ -379,7 +379,7 @@ export default function Page() {
           </div>
 
           {/* Heading Section */}
-          <h1 className="text-3xl sm:text-4xl lg:text-[50px] xl:text-6xl  font-normal tracking-wider text-black">
+          <h1 className="text-3xl sm:text-4xl lg:text-[55px] xl:text-6xl  font-normal tracking-wider text-black">
             OE Parts Distribution
           </h1>
         </div>
