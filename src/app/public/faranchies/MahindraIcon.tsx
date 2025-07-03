fd1cf740bc723dd62087d52f74789cc3cc4bb950
Roshan <1js20is089@gmail.com>
2025-07-04 00:26:47 +0530

    change 1

--- src/app/public/faranchies/MahindraIcon.tsx
+++ src/app/public/faranchies/MahindraIcon.tsx
@@ -5,7 +5,7 @@ const MahindraIcon = () => {
     <svg
       width="200"
       height="75"
-      viewBox="0 0 200 85"
+      viewBox="-9 0 200 85"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
