e908a12ff5a94be69c197399f492861769455ec4
shubhamsingh5216 <shubhamsingh02022004@gmail.com>
2025-11-11 14:31:54 +0530

    banner

--- src/app/financial-services/page.tsx
+++ src/app/financial-services/page.tsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-stretch w-full min-h-screen bg-gradient-to-b from-[#240135] via-[#9440BC] to-[#CD62FF] bg-[length:100%_300%] bg-no-repeat text-white bg-black">
-      <section className="w-full px-4 sm:px-6 md:px-8 lg:px-32 z-10 mx-auto py-12 sm:py-16 md:py-20">
+      <section className="w-full px-4 sm:px-6 md:px-8 lg:px-32 z-10 mx-auto py-12 sm:py-16 md:py-16">
         {/* HEADER SECTION */}
        <header className="w-full flex flex-col items-center justify-center mb-8 sm:mb-1 h-[60px] sm:h-[80px] md:h-[70px] text-center px-4">
   <div className="w-full flex justify-center items-center">
@@ -19,7 +19,7 @@ export default function Home() {
       width={320}
       height={150}
       priority
-      className="mx-auto w-[220px] sm:w-[240px] md:w-[300px] lg:w-[320px] h-auto"
+      className="mx-auto w-[220px] sm:w-[240px] md:w-[300px] lg:w-[300px] h-auto"
     />
   </div>
 </header>
@@ -28,12 +28,12 @@ export default function Home() {
       </section>
 
       {/* BUILDING IMAGE */}
-      <div className="relative w-full h-[200px] sm:h-[200px] md:h-[280px] lg:h-[450px] xl:h-[550px]">
+      <div className="relative w-full h-[200px] sm:h-[200px] md:h-[280px] lg:h-[450px] xl:h-[700px]">
         <Image
-          src="/imagesanime/Gove_finance_new.jpg"
+          src="/imagesanime/Govefinancenew.png"
           alt="Historic building"
           fill
-          className="object-cover md:object-contain"
+          className="object-cover md:object-cover"
           priority
         />
       </div>
