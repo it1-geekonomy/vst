d16fe44f5859715059bd2df763b67cb3fbd637d3
shubhamsingh5216 <shubhamsingh02022004@gmail.com>
2025-11-11 11:08:54 +0530

    responsive

--- src/app/financial-services/page.tsx
+++ src/app/financial-services/page.tsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-stretch w-full min-h-screen bg-gradient-to-b from-[#240135] via-[#9440BC] to-[#CD62FF] bg-[length:100%_300%] bg-no-repeat text-white">
-      <section className="w-full px-4 sm:px-6 md:px-8 lg:px-32 z-10 mx-auto py-12 sm:py-16 md:py-20">
+      <section className="w-full px-4 sm:px-6 md:px-8 lg:px-32 z-10 mx-auto py-12 sm:py-16 md:py-16 mt-4">
         {/* HEADER SECTION */}
-       <header className="w-full flex flex-col items-center justify-center mb-8 sm:mb-10 md:mb-12 h-[80px] sm:h-[100px] md:h-[110px] text-center px-4">
+       <header className="w-full flex flex-col items-center justify-center mb-8 sm:mb-10 md:mb-12 h-[80px] sm:h-[100px] md:h-[80px] lg:h-[90px] xl:h-[110px] text-center px-4 md:mt-6 lg:mt-8">
   <div className="w-full">
     <Image
       src="/imagesanime/govelogo.png"
@@ -25,18 +25,20 @@ export default function Home() {
       </section>
 
       {/* BUILDING IMAGE */}
-      <div className="relative w-full h-[360px] sm:h-[520px] lg:h-[800px]">
+      <div className="relative w-full h-[200px] sm:h-[200px] md:h-[280px] lg:h-[450px] xl:h-[550px]">
         <Image
           src="/imagesanime/historicbuilding.jpg"
           alt="Historic building"
           fill
-          className="object-cover"
+          className="object-cover md:object-contain"
           priority
         />
       </div>
 
       {/* TAGLINE */}
-      <section className="relative w-full text-center mt-12 sm:mt-16 lg:mt-24">
+      <section className="relative w-full text-center mt-6 sm:mt-6 md:mt-6 lg:mt-6 xl:mt-6 2xl:mt-12">
+
+
         <div className="relative w-full h-[220px] sm:h-[280px] lg:h-[320px]">
           <Image
             src="/imagesanime/carfinance.jpg"
